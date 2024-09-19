75d9bc2726555943fe4769620a3514cf3ea8b5a8
Kostyantyn Karimov <user.name>
2024-09-20 01:31:00 +0300

    project init

---
+++ backend/index.js
@@ -0,0 +1,9 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  console.log("Request received");
+});
+
+server.listen(3001, "localhost", () => {
+  console.log("Server is on");
+});
